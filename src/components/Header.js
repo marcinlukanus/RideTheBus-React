f8102533e8a8e19f6fbd39451db00a690d8805cd
unknown <mlukanus@gmail.com>
2021-07-19 16:55:00 -0500

    Adds Header component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to RideTheBus!</h1>
+            <p>
+                <i>A single-player drinking game created by Marcin Lukanus.</i>
+            </p>
+            <p>
+                <a
+                    href="https://github.com/marcinlukanus/RideTheBus-React" 
+                    target="_blank" 
+                    rel="noreferrer"
+                >
+                    GitHub Repo
+                </a>
+            </p>
+        </div>
+    )
+}
+
+export default Header
