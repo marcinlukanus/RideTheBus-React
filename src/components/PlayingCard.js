f15a4f58ecbdfbb167e9d122e323b7b666c23822
Marcin Lukanus <mlukanus@gmail.com>
2021-07-19 19:38:23 -0500

    Adds PlayingCard component with card back image source

---
+++ src/components/PlayingCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+    margin-left: 1.5em;
+    margin-right: 1.5em;
+`
+
+const Card = styled.img`
+    height: 314px
+    width: 226px
+`
+
+const PlayingCard = () => {
+    return (
+        <CardContainer>
+            <Card src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Card_back_06.svg/209px-Card_back_06.svg.png" />
+        </CardContainer>
+    )
+}
+
+export default PlayingCard
